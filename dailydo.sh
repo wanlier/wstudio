b07d5580059cb652304a299d06dfa41bbdfc8a92
wanlier <471584240@qq.com>
2017-02-18 22:58:24 +0800

    better

--- dailydo.sh
+++ dailydo.sh
@@ -15,7 +15,7 @@ git pull
 # 请按下面这条命令的格式加入自己要自动执行的策略
 # 先把你自己要运行的Rmd文件保存在wstudio文件夹中，比如你要执行的策略文件为lilei7525.Rmd
 # 然后只需要复制黏贴下面这条命令，把"hanmeimei1314.Rmd"改成"lilei7525.Rmd"就好了
-Rscript -e 'library("knitr"); knit2html("hanmeimei1314.Rmd",force_v1 = TRUE)'
+Rscript -e 'library("knitr"); knit2html("\\*.Rmd",force_v1 = TRUE)'
 
 
 ##################################################################################
